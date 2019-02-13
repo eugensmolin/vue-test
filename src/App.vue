2cb8f6e6b872af185ca0c086ad3f2d1a95ab6868
Jackie665 <tonylaureano665@gmail.com>
2019-02-13 16:54:53 +0200

    vue.js

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <h2>{{ msg }}</h2>
+        <div v-for="elem in arr">
+            {{ elem.name }} - {{ elem.years }}
+        </div>
+    </div>
+    <!--<counter></counter>-->
+</template>
+
+<script>
+
+    // Vue.component('counter', require('./components/Counter').default);
+
+    var arr = [
+        {
+            name: 'Stan',
+            years: 32
+        },
+        {
+            name: 'Estas',
+            years: 65
+        }
+    ];
+
+    export default {
+        name: "app",
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App',
+                arr: arr
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Counter component!</h2>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "Counter"
+    }
+
+</script>
+
+<style scoped>
+
+</style>
