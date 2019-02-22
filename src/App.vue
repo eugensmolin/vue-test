553242b93ec513ac7e5367dbac1e2cf57995eb2b
Jackie665 <tonylaureano665@gmail.com>
2019-02-22 10:19:10 +0200

    Visible element.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,61 @@
 <template>
     <div class="container">
-        <div class="logo">
+        <!--<div class="logo">
             <img src="./assets/logo.png">
-        </div>
+        </div>-->
 
         <div class="content">
-            <input type="text" @input="changeInput">
-            <h3 class="result">
-                {{ result }}
-            </h3>
-            <!-- ===== -->
-            <h3>{{ name }} {{ name.split('') }}</h3>
-            <!-- ===== -->
-            <a :href="url">Google</a>
+            Counter: {{ counter }}
+            <button @click="addNum(1, $event)">Click</button>
+            <button @click="addNum(5, $event)">Click</button>
+
+            <h2 @mouseover="hover">Header hover!</h2>
+            <hr>
+            <h2>
+                <a href="http://google.com" target="_blank" @click.prevent="">Google</a>
+            </h2>
+            <h2 @mousemove="mouseMove">
+                X: {{ x }} | Y: {{ y }}
+                <span @mousemove.stop="">Stop</span>
+            </h2>
+            <hr>
+            <input @keyup.enter.space="inpValue">
+            <hr>
+            <input v-model="inputValue">
+            <h3>{{ inputValue }}</h3>
+            <hr>
+            <!--<input v-model="color">
+            <div>
+                <div class="square"
+                     :class="[color, {'red': isActive}]"
+                ></div>
+
+                <div class="circle"
+                     @click="isActive = !isActive"
+                     :class="cssClasses"
+                ></div>
+            </div>-->
+            <hr>
+            <input v-model="color">
+            <input v-model="height">
+            <div>
+                <div
+                        class="circle"
+                        :style="circleClasses"
+                ></div>
+                <div
+                        class="circle"
+                        :style="[circleClasses, {'width': height + 'px'}]"
+                ></div>
+            </div>
+            <hr>
+
+            
+
         </div>
     </div>
 </template>
+
 
 <script>
 
@@ -24,18 +64,54 @@
         name: "app",
         data() {
             return {
-                result: 'Hello Vue!',
-                name: 'Hello Vue',
-                url: 'http://google.com'
+                counter: 0,
+                x: 0,
+                y: 0,
+                inputValue: 'value',
+                isActive: false,
+                color: '',
+                height: 30
+            }
+        },
+        computed: {
+            cssClasses() {
+                return {
+                    'red': this.isActive,
+                    'blue': !this.isActive
+                }
+            },
+            circleClasses() {
+                return {
+                    'background-color': this.color,
+                    'height': this.height + 'px'
+                }
             }
         },
         methods: {
-            changeInput(event) {
-                this.result = event.target.value;
+            addNum(num, e) {
+                this.counter += num;
+
+                if (num === 1) {
+                    e.target.style.color = 'green';
+                } else if (num === 5) {
+                    e.target.style.color = 'red';
+                }
+            },
+            hover(e) {
+                e.target.style.background = '#ccc';
+            },
+            mouseMove(e) {
+                this.x = e.clientX;
+                this.y = e.clientY;
+                // console.log(e);
+            },
+            inpValue(e) {
+                console.log(e.target.value);
             }
         }
     }
 </script>
+
 
 <style lang="scss">
     .container {
@@ -48,6 +124,32 @@
 
         .content {
             text-align: center;
+        }
+
+        .square,
+        .circle {
+            display: inline-block;
+            width: 30px;
+            height: 30px;
+            border: 1px solid #555;
+            box-shadow: 0 0 8px #999;
+            margin-top: 10px;
+        }
+
+        .circle {
+            border-radius: 50px;
+        }
+
+        .red {
+            background-color: firebrick;
+        }
+
+        .blue {
+            background-color: cornflowerblue;
+        }
+
+        .green {
+            background-color: seagreen;
         }
     }
 </style>
